b26b89db52efeed3485241f8bf8d80750440c411
Vishal Kumar Singh <vishalkumar70854@gmail.com>
2024-07-23 23:10:24 +0530

    build should fail and trigger email

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 	
-	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+//	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
 	@Test
 	void contextLoads() {
